d5dda47db85a9ffd1bb040ec4f11c76847446d8b
Anupam Basu <nupebasu@gmail.com>
2014-05-26 19:49:37 -0500

    v1.5
    deleting migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_many :photos
   has_many :favorites
   has_many :favorite_photos, :through => :favorites, :source => :photo
-
+  belongs_to
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
